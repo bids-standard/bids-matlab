1c52ad2c789b01a7ac70d44ff9658cb075d012ce
Remi Gau <6961185+Remi-Gau@users.noreply.github.com>
2019-07-19 16:29:51 +0200

    small fix in a filter that skipped json files when they were in the root folder (#8)
    small fix in a filter that skipped json files when they were in the root folder

--- +bids/private/get_metadata.m
+++ +bids/private/get_metadata.m
@@ -10,7 +10,7 @@ function meta = get_metadata(filename, pattern)
 % Copyright (C) 2018--, BIDS-MATLAB developers
 
 
-if nargin == 1, pattern = '^.*_%s\\.json$'; end
+if nargin == 1, pattern = '^.*%s\\.json$'; end
 pth = fileparts(filename);
 p = parse_filename(filename);
 
