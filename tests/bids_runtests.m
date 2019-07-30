192538d5999838ddf4c33e8dba072464ebb53f54
gllmflndn <gllmflndn@users.noreply.github.com>
2019-07-30 10:35:19 +0100

    Install BIDS-Validator

--- tests/bids_runtests.m
+++ tests/bids_runtests.m
@@ -15,17 +15,29 @@ function results = bids_runtests(pth)
 if ~nargin, pth = fileparts(mfilename('fullpath')); end
 
 d = dir(pth);
-d(arrayfun(@(x) x.isdir || ~strncmp(x.name,'test_',5),d)) = [];
+d([d.isdir]) = [];
+d(arrayfun(@(x) isempty(regexp(x.name,'^test_.*\.m$','once')),d)) = [];
 
 results = struct('Passed',{},'Failed',{},'Incomplete',{},'Duration',{});
 for i=1:numel(d)
     results(i).Failed = false;
+    results(i).Passed = false;
+    results(i).Incomplete = false;
+    tstart = tic;
     try
-        fprintf('%s\n',d(i).name);
+        fprintf('%s\n',d(i).name(1:end-2));
         feval(d(i).name(1:end-2));
+        results(i).Passed = true;
     catch
         results(i).Failed = true;
         err = lasterror;
         disp(err.message);
     end
+    results(i).Duration = toc(tstart);
+end
+
+if ~nargout
+    fprintf(['Totals (%d tests):\n\t%d Passed, %d Failed, %d Incomplete.\n' ...
+        '\t%f seconds testing time.\n\n'],numel(results),nnz([results.Passed]),...
+        nnz([results.Failed]),nnz([results.Incomplete]),sum([results.Duration]));
 end
