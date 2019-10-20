7774c663eab07658c45ddfa889c06b889d2a80a6
Remi Gau <remi_gau@hotmail.com>
2019-10-21 00:14:20 +0200

    Add comments to get_medata and parse_filename

--- +bids/private/get_metadata.m
+++ +bids/private/get_metadata.m
@@ -11,22 +11,38 @@ function meta = get_metadata(filename, pattern)
 
 
 if nargin == 1, pattern = '^.*%s\\.json$'; end
+
 pth = fileparts(filename);
 p = parse_filename(filename);
 
 meta = struct();
 
 N = 3;
+
+% there is a session level in the hierarchy
 if isfield(p,'ses') && ~isempty(p.ses)
-    N = N + 1; % there is a session level in the hierarchy
+    N = N + 1; 
 end
     
+% loop from the directory of where the file of interest is back to the 
+% top level of the BIDS hierarchy
 for n=1:N
-    metafile = file_utils('FPList',pth, sprintf(pattern,p.type));
+    
+    % list the potential metadata files associated with this file suffix type
+    % default assumes it is a json file
+    metafile = file_utils('FPList', pth, sprintf(pattern, p.type));
+    
     if isempty(metafile), metafile = {}; else metafile = cellstr(metafile); end
+    
+    % for all those files we find which one is potentially associated with
+    % the file of interest
     for i=1:numel(metafile)
+        
         p2 = parse_filename(metafile{i});
         fn = setdiff(fieldnames(p2),{'filename','ext','type'});
+        
+        % checks if this metadata file contains the same entity-label pairs as its
+        % data file counterpart
         ismeta = true;
         for j=1:numel(fn)
             if ~isfield(p,fn{j}) || ~strcmp(p.(fn{j}),p2.(fn{j}))
@@ -34,6 +50,9 @@ for n=1:N
                 break;
             end
         end
+        
+        % read the content of the metadata file if it is a json and update the 
+        % metadata concerning the file of interest otherwise stores the filename
         if ismeta
             if strcmp(p2.ext,'.json')
                 meta = update_metadata(meta,bids.util.jsondecode(metafile{i}));
@@ -41,8 +60,12 @@ for n=1:N
                 meta.filename = metafile{i};
             end
         end
+        
     end
+    
+    % go up to the parent folder
     pth = fullfile(pth,'..');
+    
 end
 
 
