1ec16e2b91c2c0b8536737a5b7b98aa720e24033
Remi Gau <remi_gau@hotmail.com>
2021-01-13 17:29:11 +0100

    make get_test_data_dir verbose only on failure

--- tests/utils/get_test_data_dir.m
+++ tests/utils/get_test_data_dir.m
@@ -1,19 +1,24 @@
-function dataDir = get_test_data_dir()
+function data_dir = get_test_data_dir()
 
+  % default for testing locally
+  data_dir = fullfile(fileparts(mfilename('fullpath')), '..');
+    
   PLATFORM  = getenv('PLATFORM');
 
   if strcmp(PLATFORM, 'GITHUB_ACTIONS')
 
-    dataDir = '/github/workspace/tests/';
-
-  elseif isempty(PLATFORM)  % local
-
-    dataDir = fullfile(fileparts(mfilename('fullpath')), '..');
+    data_dir = '/github/workspace/tests/';
 
   end
 
-  dataDir =  fullfile(dataDir, 'bids-examples');
-
-  fprintf(1, '\n Reading bids-examples data from: %s\n', dataDir);
+  data_dir =  fullfile(data_dir, 'bids-examples');
+  
+  if exist(data_dir, 'dir') ~= 7
+      msg = sprintf([...
+          'The bids-example folder %s was not found.\n', ...
+          'Install it in the tests folder with:\n', ...
+          'git clone git://github.com/bids-standard/bids-examples.git --depth 1']);
+      error(msg); %#ok<SPERR>
+  end
 
 end
