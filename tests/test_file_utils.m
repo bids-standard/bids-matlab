2cb2a4ddb1e43540f61ebbc96970c02eca12846c
Remi Gau <remi_gau@hotmail.com>
2021-01-14 15:33:04 +0100

    add test for file_utils
    when you are afraid of breaking things, write a test

---
+++ tests/test_file_utils.m
@@ -0,0 +1,53 @@
+function test_file_utils()
+
+  % test to get certain part of a filename
+  % {'path', 'basename', 'ext', 'filename', 'cpath', 'fpath'}
+
+  str = fullfile('folder', 'filename.extension');
+
+  p = bids.internal.file_utils(str, 'path');
+  assert(isequal(p, 'folder'));
+
+  p = bids.internal.file_utils(str, 'fpath');
+  assert(isequal(p, fullfile(pwd, 'folder')));
+
+  filename = bids.internal.file_utils(str, 'basename');
+  assert(isequal(filename, 'filename'));
+
+  ext = bids.internal.file_utils(str, 'ext');
+  assert(isequal(ext, 'extension'));
+
+  str = fullfile('folder', 'subfolder', '..', 'filename.extension');
+  cpath = bids.internal.file_utils(str, 'cpath');
+  assert(isequal(cpath, ...
+                 fullfile(pwd, 'folder', 'filename.extension')));
+
+  % test to set certain part of a filename
+  % {'path', 'basename', 'ext', 'filename', 'prefix', 'suffix'}
+
+  str = fullfile('folder', 'filename.extension');
+
+  new_str = bids.internal.file_utils(str, 'ext', 'newext');
+  assert(isequal(new_str, fullfile('folder', 'filename.newext')));
+
+  new_str = bids.internal.file_utils(str, 'basename', 'new_name');
+  assert(isequal(new_str, fullfile('folder', 'new_name.extension')));
+
+  new_str = bids.internal.file_utils(str, 'filename', 'new_name.newext');
+  assert(isequal(new_str, fullfile('folder', 'new_name.newext')));
+
+  new_str = bids.internal.file_utils(str, 'prefix', 'pre_');
+  assert(isequal(new_str, fullfile('folder', 'pre_filename.extension')));
+
+  new_str = bids.internal.file_utils(str, 'suffix', '_suffix');
+  assert(isequal(new_str, fullfile('folder', 'filename_suffix.extension')));
+
+  new_str = bids.internal.file_utils(str, 'path', fullfile(pwd, 'new_folder'));
+  assert(isequal(new_str, fullfile(pwd, 'new_folder', 'filename.extension')));
+
+  new_str = bids.internal.file_utils(str, ...
+                                     'prefix', 'pre_', ...
+                                     'suffix', '_suffix');
+  assert(isequal(new_str, fullfile('folder', 'pre_filename_suffix.extension')));
+
+end
