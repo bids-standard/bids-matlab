456e851120fe7174c31d5577831ba80bca3d5d76
Remi Gau <remi_gau@hotmail.com>
2021-01-16 12:16:10 +0100

    create test for load_schema

---
+++ tests/test_load_schema.m
@@ -0,0 +1,13 @@
+function test_load_schema()
+    
+    SCHEMA_DIR = fullfile(fileparts(mfilename('fullpath')), 'schema');
+    
+    schema = bids.internal.load_schema(SCHEMA_DIR);
+    
+    assert(isfield(schema, 'base'));
+    assert(isfield(schema, 'subfolder_1'));
+    assert(isfield(schema.subfolder_1, 'sub'));
+%     assert(isfield(schema.subfolder_2, 'subfolder_3'));
+    assert(~isfield(schema, 'subfolder_4'));
+
+end
