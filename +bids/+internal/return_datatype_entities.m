e14da01f60cd175d58afe404a1a5ec07d7409334
Remi Gau <remi_gau@hotmail.com>
2021-02-04 06:02:54 +0100

    update return_datatype_entities to new schema structure

--- +bids/+internal/return_datatype_entities.m
+++ +bids/+internal/return_datatype_entities.m
@@ -1,5 +1,11 @@
 function entities = return_datatype_entities(datatype)
 
-  entities = fieldnames(datatype.entities);
+  schema = bids.internal.load_schema();
+
+  entity_names = fieldnames(datatype.entities);
+
+  for i = 1:size(entity_names, 1)
+    entities{1, i} = schema.entities.(entity_names{i}).entity; %#ok<*AGROW>
+  end
 
 end
