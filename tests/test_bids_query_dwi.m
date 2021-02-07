888ad556a21bdb4a9b4ea7ece41a563c9e0da4b3
Remi Gau <remi_gau@hotmail.com>
2021-02-07 13:08:33 +0100

    add test dwi

---
+++ tests/test_bids_query_dwi.m
@@ -0,0 +1,26 @@
+function test_suite = test_bids_query_dwi %#ok<*STOUT>
+  try % assignment of 'localfunctions' is necessary in Matlab >= 2016
+    test_functions = localfunctions(); %#ok<*NASGU>
+  catch % no problem; early Matlab versions can use initTestSuite fine
+  end
+  initTestSuite;
+end
+
+function test_bids_query_dwi_basic()
+  %
+  %   eeg queries
+  %
+
+  pth_bids_example = get_test_data_dir();
+
+  %%
+  BIDS = bids.layout(fullfile(pth_bids_example, 'ds000117'));
+
+  modalities = {'anat',    'beh',    'dwi',    'fmap',    'func',    'meg'};
+  assertEqual(bids.query(BIDS, 'modalities'), modalities);
+
+  types = {'T1w', 'bold', 'dwi', 'events', 'magnitude1', 'magnitude2', 'meg', 'phasediff'};
+  % Missing: 'headshappe', 'FLASH'
+  assertEqual(bids.query(BIDS, 'types'), types);
+
+end
