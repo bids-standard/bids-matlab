b558a0b6f901e3ff65477f6c09fdf74e1d25a7f1
Remi Gau <remi_gau@hotmail.com>
2021-02-18 19:13:30 +0100

    add test for row wise structure

--- tests/test_tsvwrite.m
+++ tests/test_tsvwrite.m
@@ -4,46 +4,37 @@ function test_suite = test_tsvwrite %#ok<*STOUT>
   catch % no problem; early Matlab versions can use initTestSuite fine
   end
   initTestSuite;
-end
 
-function test_tsvwrite_basic()
-  % Test the tsvwrite function
-  % __________________________________________________________________________
   %
-  % BIDS (Brain Imaging Data Structure): https://bids.neuroimaging.io/
-  %   The brain imaging data structure, a format for organizing and
-  %   describing outputs of neuroimaging experiments.
-  %   K. J. Gorgolewski et al, Scientific Data, 2016.
-  % __________________________________________________________________________
+  % Small test to ensure that the functionality of the tsvwrite function
   %
   % Copyright (C) 2020, Remi Gau
   % Copyright (C) 2020--, BIDS-MATLAB developers
   %
-  %
-  % Small test to ensure that the functionality of the tsvwrite function
+
+end
+
+function test_tsvwrite_row_wise_structure()
+
   pth = fileparts(mfilename('fullpath'));
-
-  %% test tsvread on tsv file using structure input
-
-  % ---- set up
 
   tsv_file = fullfile(pth, 'sub-01_task-STRUCTURE_events.tsv');
 
-  logFile.onset = [2; NaN];
-  logFile.trial_type = {'motion_up'; 'static'};
-  logFile.duration = [1; 4];
-  logFile.speed = [NaN; 4];
-  logFile.is_fixation = {'true'; '3'};
+  logFile(1, 1).onset = 2;
+  logFile(1, 1).trial_type = 'motion_up';
+  logFile(1, 1).duration = 1;
+  logFile(1, 1).speed = [];
+  logFile(1, 1).is_fixation = true;
 
-  % Leads to trouble
-  %   logFile.is_fixation = {'true';3};
-  %   logFile.is_fixation = {true;3};
+  logFile(2, 1).onset = NaN;
+  logFile(2, 1).trial_type = 'static';
+  logFile(2, 1).duration = 4;
+  logFile(2, 1).speed = 4;
+  logFile(2, 1).is_fixation = 3;
 
   bids.util.tsvwrite(tsv_file, logFile);
 
-  % ---- test section
-
-  % read the file
+  % read the file &
   % check the extra columns of the header and some of the content
 
   FID = fopen(tsv_file, 'r');
@@ -64,10 +55,55 @@ function test_tsvwrite_basic()
   % check values entered properly: logFile.is_fixation(2)
   assertEqual(C{5}{3}, '3');
 
-  %% test tsvread on tsv file using cell input
-  % TO DO?
+end
 
-  %% test tsvread on tsv file using array input
+function test_tsvwrite_basic()
+
+  pth = fileparts(mfilename('fullpath'));
+
+  tsv_file = fullfile(pth, 'sub-01_task-STRUCTURE_events.tsv');
+
+  logFile.onset = [2; NaN];
+  logFile.trial_type = {'motion_up'; 'static'};
+  logFile.duration = [1; 4];
+  logFile.speed = [NaN; 4];
+  logFile.is_fixation = {'true'; '3'};
+
+  % Leads to trouble
+  %   logFile.is_fixation = {'true';3};
+  %   logFile.is_fixation = {true;3};
+
+  bids.util.tsvwrite(tsv_file, logFile);
+
+  % read the file &
+  % check the extra columns of the header and some of the content
+
+  FID = fopen(tsv_file, 'r');
+  C = textscan(FID, '%s%s%s%s%s', 'Delimiter', '\t', 'EndOfLine', '\n');
+
+  % check header
+  assertEqual(C{4}{1}, 'speed');
+
+  % check that empty values are entered as NaN: logFile.speed(1)
+  assertEqual(C{4}{2}, 'n/a');
+
+  % check that missing fields are entered as NaN: logFile.speed(2)
+  assertEqual(C{4}{3}, '4');
+
+  % check that NaN are written as : logFile.onset(2)
+  assertEqual(C{1}{3}, 'n/a'); %
+
+  % check values entered properly: logFile.is_fixation(2)
+  assertEqual(C{5}{3}, '3');
+
+end
+
+% TODO test tsvread on tsv file using cell input
+
+function test_tsvwrite_array
+
+  pth = fileparts(mfilename('fullpath'));
+
   tsv_file = fullfile(pth, 'sub-01_task-ARRAY_events.tsv');
 
   a = [ ...
