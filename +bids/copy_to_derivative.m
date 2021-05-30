0c33ffc82adb7b9e291ec559f34a219e4777c9dc
Remi Gau <remi_gau@hotmail.com>
2021-05-30 22:50:33 +0200

    fix copy to derivatives for windows

--- +bids/copy_to_derivative.m
+++ +bids/copy_to_derivative.m
@@ -289,35 +289,33 @@ function copy_with_symlink(src, target, verbose)
     fprintf(1, '\n copying %s --> %s', src, target);
   end
 
-  try
-    status = system( ...
-                    sprintf('%s %s %s', ...
-                            command, ...
-                            src, ...
-                            target));
-
-    if status > 0
-      msg = [ ...
-             'Copying data with system command failed: ' ...
-             'Are you running Windows?\n', ...
-             'Will use matlab/octave copyfile command instead.\n', ...
-             'May be an issue if your data set contains symbolic links' ...
-             '(e.g. if you use datalad or git-annex.)'];
-      bids.internal.error_handling(mfilename, 'copyError', msg, true, verbose);
+    if  isunix
+        status = system( ...
+                        sprintf('%s %s %s', ...
+                                command, ...
+                                src, ...
+                                target));
+        if status > 0
+          msg = ['Copying data with system command failed: ' ...
+                 'Will use matlab/octave copyfile command instead.\n', ...
+                 'May be an issue if your data set contains symbolic links' ...
+                 '(e.g. if you use datalad or git-annex.)'];
+          bids.internal.error_handling(mfilename, 'copyError', msg, true, verbose);
+          use_copyfile(src, target, verbose);
+        end
+        
+    else
+        use_copyfile(src, target, verbose);
     end
 
-  catch
+end
 
-    fprintf(1, 'Using octave/matlab to copy files.');
+function use_copyfile(src, target, verbose)
     [status, message, messageId] = copyfile(src, target);
     if ~status
       msg = [messageId ': ' message];
-      bids.internal.error_handling(mfilename, 'copyError', msg, true, verbose);
-      return
+      bids.internal.error_handling(mfilename, 'copyError', msg, false, verbose);
     end
-
-  end
-
 end
 
 function copy_dependencies(file, BIDS, derivatives_folder, unzip, force, skip_dep, verbose)
