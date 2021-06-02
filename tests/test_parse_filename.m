2752d6d5daea51cfa95936508ec0c4e677544215
Remi Gau <remi_gau@hotmail.com>
2021-06-02 12:06:39 +0200

    add test

--- tests/test_parse_filename.m
+++ tests/test_parse_filename.m
@@ -23,7 +23,7 @@ end
 
 function test_parse_filename_prefix()
 
-  filename = '../sub-16/anat/asub-16_task-rest_run-1_bold.nii';
+  filename = 'asub-16_task-rest_run-1_bold.nii';
   output = bids.internal.parse_filename(filename);
 
   expected = struct( ...
@@ -34,6 +34,25 @@ function test_parse_filename_prefix()
                     'entities', struct('sub', '16', ...
                                        'task', 'rest', ...
                                        'run', '1'));
+
+  assertEqual(output, expected);
+
+  expectedEntities = fieldnames(expected.entities);
+  entities = fieldnames(output.entities);
+  assertEqual(entities, expectedEntities);
+
+  % repeated entity
+  filename = 'asub-16_task-rest_wsub-1_bold.nii';
+  output = bids.internal.parse_filename(filename);
+
+  expected = struct( ...
+                    'filename', 'asub-16_task-rest_wsub-1_bold.nii', ...
+                    'suffix', 'bold', ...
+                    'prefix', 'a', ...
+                    'ext', '.nii', ...
+                    'entities', struct('sub', '16', ...
+                                       'task', 'rest', ...
+                                       'wsub', '1'));
 
   assertEqual(output, expected);
 
