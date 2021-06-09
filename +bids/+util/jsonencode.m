c64c01d83e4058f10d8b53c375842c9adf7ff0ff
Remi Gau <remi_gau@hotmail.com>
2021-06-09 11:49:25 +0200

    apply mh_style linting

--- +bids/+util/jsonencode.m
+++ +bids/+util/jsonencode.m
@@ -1,42 +1,41 @@
 function varargout = jsonencode(varargin)
   %
   % Encode data to JSON-formatted file
-  %  
+  %
   % USAGE::
   %
   %   bids.util.jsonencode(filename, json, opts)
-  %  
+  %
   % :param filename: JSON filename
   % :type filename: string
   % :param json: JSON structure
   % :type json: structure
-  %  
-  %  
+  %
+  %
   % USAGE::
-  %  
+  %
   %   S = bids.util.jsonencode(json, opts)
-  %  
+  %
   % :param json: JSON structure
   % :type json: structure
   %
   % :returns: - :S: (string) serialized JSON structure
-  %  
-  %  
+  %
+  %
   % :param opts: optional parameters
   % :type opts: structure
-  %  
+  %
   %   - ``Indent``: string to use for indentation; [Default: ``''``]
   %   - ``ReplacementStyle``: string to control how non-alphanumeric
   %                       characters are replaced; [Default: ``'underscore'``]
   %   - ``ConvertInfAndNaN``: encode ``NaN``, ``Inf`` and ``-Inf`` as ``"null"``;
   %                       [Default: ``true``]
-  %  
+  %
   % (C) Copyright 2018 Guillaume Flandin, Wellcome Centre for Human Neuroimaging
   % (C) Copyright 2018 BIDS-MATLAB developers
 
-if ~nargin
+  if ~nargin
     error('Not enough input arguments.');
-else
+  else
     [varargout{1:nargout}] = jsonwrite(varargin{:}); % JSONio copy - always exist
-end
-
+  end
