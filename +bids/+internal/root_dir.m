4c02c825d7c1e9a75983546b1919915093afb598
Beliy Nikita <beliy.nikita@uliege.be>
2022-01-14 10:22:04 +0100

    Fixed persistent variable in root_dir

--- +bids/+internal/root_dir.m
+++ +bids/+internal/root_dir.m
@@ -1,10 +1,11 @@
 function pth = root_dir()
   %
   % (C) Copyright 2021 BIDS-MATLAB developers
-  persistent pth;
-  if isempty(pth)
-    pth = fullfile(fileparts(mfilename('fullpath')), '..', '..');
-    pth = bids.internal.file_utils(fullfile(fileparts(mfilename('fullpath')), '..', '..'), 'cpath');
+  persistent path;
+  if isempty(path)
+    path = fullfile(fileparts(mfilename('fullpath')), '..', '..');
+    path = bids.internal.file_utils(fullfile(fileparts(mfilename('fullpath')), '..', '..'), 'cpath');
   end
+  pth = path;
 
 end
