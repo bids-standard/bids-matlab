d822612e53780571ac548b051eb53bf63b9eb4a6
Remi Gau <remi_gau@hotmail.com>
2022-05-16 12:39:10 +0200

    try silencing the tons of octave warnings

--- tests/test_transformers/test_transformers_compute.m
+++ tests/test_transformers/test_transformers_compute.m
@@ -2,6 +2,10 @@ function test_suite = test_transformers_compute %#ok<*STOUT>
   %
   % (C) Copyright 2022 Remi Gau
 
+  if bids.internal.is_octave
+    warning off;
+  end
+
   try % assignment of 'localfunctions' is necessary in Matlab >= 2016
     test_functions = localfunctions(); %#ok<*NASGU>
   catch % no problem; early Matlab versions can use initTestSuite fine
