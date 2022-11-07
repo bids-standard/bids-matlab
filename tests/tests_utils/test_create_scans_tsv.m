b35592be8844eaac0fb6044c06c7920401f8365f
Remi Gau <remi_gau@hotmail.com>
2022-11-07 15:08:51 +0100

    fix some test

--- tests/tests_utils/test_create_scans_tsv.m
+++ tests/tests_utils/test_create_scans_tsv.m
@@ -10,12 +10,18 @@ function test_create_scans_tsv_basic_no_session()
 
   bids_path = fullfile(get_test_data_dir(), 'asl001');
 
+  [sts, msg] = bids.validate(bids_path,  '--config.ignore=99 --ignoreNiftiHeaders'); %#ok<*ASGLU>
+  assertEqual(sts, 0);
+
   output_filenames = bids.util.create_scans_tsv(bids_path, 'verbose', true);
 
   assertEqual(numel(output_filenames), 1);
   assertEqual(exist(fullfile(bids_path, output_filenames{1}), 'file'), 2);
   content = bids.util.tsvread(fullfile(bids_path, output_filenames{1}));
   assertEqual(fieldnames(content), {'filename'; 'acq_time'; 'comments'});
+
+  [sts, msg] = bids.validate(bids_path,  '--config.ignore=99 --ignoreNiftiHeaders');
+  assertEqual(sts, 0);
 
   teardown(bids_path, output_filenames);
 
@@ -25,12 +31,18 @@ function test_create_scans_tsv_basic()
 
   bids_path = fullfile(get_test_data_dir(), 'ds000117');
 
+  [sts, msg] = bids.validate(bids_path,  '--config.ignore=99 --ignoreNiftiHeaders');
+  assertEqual(sts, 0);
+
   output_filenames = bids.util.create_scans_tsv(bids_path, 'verbose', true);
 
   assertEqual(numel(output_filenames), 16);
   assertEqual(exist(output_filenames{1}, 'file'), 2);
   content = bids.util.tsvread(output_filenames{1});
   assertEqual(fieldnames(content), {'filename'; 'acq_time'; 'comments'});
+
+  sts = bids.validate(bids_path,  '--config.ignore=99 --ignoreNiftiHeaders');
+  assertEqual(sts, 0);
 
   teardown(bids_path, output_filenames);
 
