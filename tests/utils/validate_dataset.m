0174ceaf9172698a54494cf9ef976cd43c8441e0
Remi Gau <remi_gau@hotmail.com>
2023-05-04 22:03:12 +0200

    [DATALAD] Recorded changes

--- tests/utils/validate_dataset.m
+++ tests/utils/validate_dataset.m
@@ -1,5 +1,9 @@
 function validate_dataset(bids_path)
 
+  if ispc
+    return
+  end
+
   [sts, msg] = bids.validate(bids_path,  '--config.ignore=99 --ignoreNiftiHeaders');
   assertEqual(sts, 0);
 
