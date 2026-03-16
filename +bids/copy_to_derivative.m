86b102a6d2e6648746b848c39312fc636ae8a198
Remi Gau <remi_gau@hotmail.com>
2026-03-16 17:28:44 +0100

    only check last entry

--- +bids/copy_to_derivative.m
+++ +bids/copy_to_derivative.m
@@ -141,17 +141,25 @@ function copy_to_derivative(varargin)
     bids.util.mkdir(derivatives_folder);
   end
 
-  % In case we are copying again to the output folder, we append that info to the
-  % description; otherwise, we create a brand new dataset description for derivatives
+  % update dataset_description
+  % but only if the current pipeline is different
+  % from the one stored in the last entry
+  % of the GeneratedBy section
   ds_desc = bids.Description(args.Results.pipeline_name, BIDS);
-  descr_file = fullfile(derivatives_folder, 'dataset_description.json');
+  descr_file = fullfile(derivatives_folder, '.json');
   if isfile(descr_file)
     content = bids.util.jsondecode(descr_file);
     ds_desc = ds_desc.set_field(content);
-    if ~contains(jsonencode(content.GeneratedBy), ...
-                 sprintf('"Name":"%s"', args.Results.pipeline_name))
+
+    if iscell(content.GeneratedBy)
+      last_pipeline = content.GeneratedBy{end}.Name;
+    else
+      last_pipeline = content.GeneratedBy.Name;
+    end
+    if ~strcmp(last_pipeline, args.Results.pipeline_name)
       ds_desc = ds_desc.append('GeneratedBy', struct('Name', args.Results.pipeline_name));
     end
+
   end
 
   ds_desc.write(derivatives_folder);
